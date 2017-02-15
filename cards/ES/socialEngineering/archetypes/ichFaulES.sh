d8561804448f8bcd6909aa3bc80773380388505c
Marc jun Landolt <mail@marclandolt.ch>
2017-02-15 10:30:07 +0100

    first spanish card

--- cards/ES/socialEngineering/archetypes/ichFaulES.sh
+++ cards/ES/socialEngineering/archetypes/ichFaulES.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+find . -type f -name '*.svg' | xargs sed -i  's/Neurotizismus/neuroticismo/g'
+find . -type f -name '*.svg' | xargs sed -i  's/neurotizismus/neuroticismo/g'
 find . -type f -name '*.svg' | xargs sed -i  's/neuroticism/neuroticismo/g'
+find . -type f -name '*.svg' | xargs sed -i  's/Neuroticism/neuroticismo/g'
 find . -type f -name '*.svg' | xargs sed -i  's/Extraversion/extroversión/g'
+find . -type f -name '*.svg' | xargs sed -i  's/extraversion/extroversión/g'
 find . -type f -name '*.svg' | xargs sed -i  's/agreeableness/agradabilidad /g'
+find . -type f -name '*.svg' | xargs sed -i  's/Agreeableness/agradabilidad /g'
+find . -type f -name '*.svg' | xargs sed -i  's/Verträglichkeit/agradabilidad /g'
+find . -type f -name '*.svg' | xargs sed -i  's/verträglichkeit/agradabilidad /g'
 find . -type f -name '*.svg' | xargs sed -i  's/conscientiousness/conscientización/g'
+find . -type f -name '*.svg' | xargs sed -i  's/Conscientiousness/conscientización/g'
+find . -type f -name '*.svg' | xargs sed -i  's/Gewissenhaftigkeit/conscientización/g'
+find . -type f -name '*.svg' | xargs sed -i  's/gewissenhaftigkeit/conscientización/g'
 find . -type f -name '*.svg' | xargs sed -i  's/openness/apertura/g'
+find . -type f -name '*.svg' | xargs sed -i  's/Openness/apertura/g'
+find . -type f -name '*.svg' | xargs sed -i  's/offenheit/apertura/g'
+find . -type f -name '*.svg' | xargs sed -i  's/Offenheit/apertura/g'
 
 
 find . -type f -name '*.svg' | xargs sed -i  's/Nächstenliebe/Humanismo/g'
