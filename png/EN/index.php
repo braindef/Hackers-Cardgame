71876e4fe32312cfb0ec72c3c39f2c10f7dceaba
Your Name <you@example.com>
2019-10-04 15:38:46 +0200

    tempsave

---
+++ png/EN/index.php
@@ -0,0 +1,30 @@
+   <?php
+
+   $files = glob("*.*");
+
+  for ($i=count($files)-200; $i<count($files); $i++)
+
+{
+
+$image = $files[$i];
+$supported_file = array(
+    'gif',
+    'jpg',
+    'jpeg',
+    'png'
+);
+
+$ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+if (in_array($ext, $supported_file)) {
+    echo '<a href="./'.$image .'"><img width=200 src="'.$image .'" ></a>';
+    echo '
+';
+
+} else {
+    continue;
+ }
+
+}
+
+?>
+
