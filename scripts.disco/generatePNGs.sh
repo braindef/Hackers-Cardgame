49a2154a7b2a180d1371b1c823ffc433c38c6116
Your Name <you@example.com>
2019-10-06 03:31:59 +0200

    tempsave

--- scripts.disco/generatePNGs.sh
+++ scripts.disco/generatePNGs.sh
@@ -32,31 +32,31 @@ for i in $(cat ./PNGs.txt)
   xdotool key Ctrl+Alt+a
   sleep 1
   xdotool key Ctrl+Shift+e
-  sleep 1
+  sleep 0.3
   xdotool key Tab Tab Tab KP_Space
-  sleep 1
+  sleep 0.3
   xdotool key Tab Tab Tab Tab Tab Tab Tab Tab Tab
-  sleep 1
+  sleep 0.3
   xdotool type "1000"
   #xdotool click 1 if key-shortcuts do not work you can also use mouse commands
-  sleep 1
+  sleep 0.3
   #xdotool key Ctrl+a
   xdotool key Tab Tab Tab
-  sleep 1
+  sleep 0.3
   xdotool key Ctrl+a Delete
   pngpath=$(dirname $(pwd))/png
   echo $pngpath
   xdotool key Alt+b
-  sleep 1
+  sleep 0.3
   xdotool type "$pngpath/$1/"
   xdotool type $(basename $i .svg)
-  sleep 1
+  sleep 0.3
   xdotool type '.png'
-  sleep 1
+  sleep 0.3
   xdotool key KP_Enter
-  sleep 1
+  sleep 0.3
   #xdotool key Alt+e
-  sleep 2
+  sleep 1
   xdotool key Ctrl+w
   sleep 1
   xdotool key Alt+F4
