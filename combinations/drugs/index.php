459cfc920365832905961c5105ad6d7054a4d8fe
Your Name <you@example.com>
2019-10-12 20:21:40 +0200

    tempsave

--- combinations/drugs/index.php
+++ combinations/drugs/index.php
@@ -1,7 +1,9 @@
 
 <h1>Some drugs are maybe just Software on the mainframe</h1>
 But as long as you dont have a PhD in Medicine AND Pharmacology AND Computer Science you can not be sure about this hypothesis. And of course some chemicals have effect on the endocrine system but this does not include nor exclude that additional some thing like t1 card but 70 Years newer transmits additional data to your brain
-
+<br>
+<br>
+<br>
 
 
    <?php
